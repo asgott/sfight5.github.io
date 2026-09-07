924e58abcaf261eeac349e940523d4a74b8b95bb
asgott <asgott.m@gmail.com>
2026-09-07 10:18:25 +0200

    Refactor HP management functions in script.js

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function sayHello() {
-    alert("Hello from my character sheet!");
+let currentHP = 63;
+let maxHP = 63;
+
+let tempHP = 0;
+
+
+function changeHP(amount) {
+
+    currentHP = currentHP + amount;
+
+    if (currentHP < 0) {
+        currentHP = 0;
+    }
+
+    if (currentHP > maxHP) {
+        currentHP = maxHP;
+    }
+
+    document.getElementById("current-hp").textContent = currentHP;
+}
+
+
+function changeTempHP(amount) {
+
+    tempHP = tempHP + amount;
+
+    if (tempHP < 0) {
+        tempHP = 0;
+    }
+
+    document.getElementById("temp-hp").textContent = tempHP;
+}
+
+
+function addTestEffect() {
+
+    const effectsList = document.getElementById("effects-list");
+
+    effectsList.innerHTML = `
+        <p>Shield of Faith: +2 AC</p>
+    `;
 }
